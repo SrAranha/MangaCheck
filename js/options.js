53ebfe010e3b9d14ba1ff4c08199b1892dcb1e8c
Danilo Aranha <daniloprado7@gmail.com>
2022-12-29 22:04:17 -0300

    Added `update` option.

--- js/options.js
+++ js/options.js
@@ -1,17 +1,22 @@
 var mangas = require('./searchMangas');
 var score = require('./scoreManga');
 var show = require('./showManga');
+var update = require('./updateManga');
 var common = require('./common');
 var main = require('../main');
 
+const questionMangaName = 'Manga Name: ';
+
 exports.ShowManga = function ShowManga() {
-    common.rl.question(`${common.colors.cyan}Which manga you want to see?\n` + `${common.colors.cyan}Manga name: `, function(mangaName) {
-        show.Show(mangaName);
+    common.rl.question(`${common.colors.cyan}Which manga you want to see?\n` + questionMangaName, function(mangaName) {
+        show.Show(mangaName, true);
     });
 }
 
 exports.UpdateManga = function UpdateManga() {
-    
+    common.rl.question(`${common.colors.cyan}Which manga you want to update?\n` + questionMangaName, function(mangaName) {
+        update.Update(mangaName);
+    });
 }
 
 exports.AddManga = function AddManga() {
@@ -23,7 +28,7 @@ exports.RemoveManga = function RemoveManga() {
 }
 
 exports.ScoreManga = function ScoreManga() {
-    common.rl.question(`${common.colors.cyan}Which manga you want to add/change score?\n` + `${common.colors.cyan}Manga name: `, function(mangaName) {
+    common.rl.question(`${common.colors.cyan}Which manga you want to add/change score?\n` + questionMangaName, function(mangaName) {
         common.rl.question(`${common.colors.cyan}What score you give to ${mangaName}? `, function(newScore) {
             score.Score(mangaName, newScore);
         })
---
+++ js/updateManga.js
@@ -0,0 +1,56 @@
+var editJsonFile = require('edit-json-file');
+var score = require('./scoreManga');
+var show = require('./showManga');
+var common = require('./common');
+var main = require('../main');
+var path = require('path');
+
+/**
+ * Update data from desired manga.
+ * @param {string} mangaToUpdate manga to update.
+ */
+exports.Update = function UpdateManga(mangaToUpdate) {
+    var mangaName = common.GetManga(mangaToUpdate).name;
+    const jsonPath = path.join(__dirname, `../json/${main.jsonManga}`);
+    var jsonFile = editJsonFile(jsonPath);
+
+    show.Show(mangaName, false);
+
+    common.rl.question(`${common.colors.cyan}What you want to update?\n ${common.colors.green} Chapters | Score | Link \n`, function(whatUpdate) {
+        whatUpdate = whatUpdate.toLocaleUpperCase();
+        switch (whatUpdate) {
+        
+            case 'CHAPTERS':
+                    common.rl.question(`${common.colors.cyan}What is the latest chapter you've read?\n`, function(chapters_Read) {
+                        jsonFile.set(`${mangaName}.lastSeen`, chapters_Read);
+                        SaveShow(mangaName);
+                    });
+            break;
+        
+            case 'SCORE':
+                    common.rl.question(`${common.colors.cyan}What is the new Score?\n`, function(new_Score) {
+                        score.Score(mangaName, new_Score);
+                    });
+            break;
+        
+            case 'LINK':
+                    common.rl.question(`${common.colors.cyan}What is the new Link?\n`, function(new_Link) {
+                        console.log('newLink=>', new_Link);
+                        jsonFile.set(`${mangaName}.link`, new_Link);
+                        SaveShow(mangaName);
+                    });
+            break;
+                
+            default:
+                console.log(common.colors.red, 'Not an option, going back');
+                common.WhatNow();
+            break;
+        }
+    });
+
+    function SaveShow(mangaName) {
+        jsonFile.save();
+        show.Show(mangaName, false);
+    }
+    common.WhatNow('update');
+}
