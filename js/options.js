c6e1b33d7beff80a314aa3750d7473efdd8a8012
Danilo Aranha <daniloprado7@gmail.com>
2022-12-31 17:34:06 -0300

    New option added and minor changes. Added `AddManga` option. Ordered imports. Renamed `mangas` import to `search`.

--- js/options.js
+++ js/options.js
@@ -1,8 +1,9 @@
-var mangas = require('./searchMangas');
+var search = require('./searchMangas');
+var update = require('./updateManga');
 var score = require('./scoreManga');
 var show = require('./showManga');
-var update = require('./updateManga');
 var common = require('./common');
+var add = require('./addManga');
 var main = require('../main');
 
 const questionMangaName = 'Manga Name: ';
@@ -20,7 +21,11 @@ exports.UpdateManga = function UpdateManga() {
 }
 
 exports.AddManga = function AddManga() {
-    
+    common.rl.question(`${common.colors.cyan}What is the name of the new manga?\n` + questionMangaName, function(mangaName) {
+        common.rl.question(`${common.colors.cyan}And the link to it?\n`, function(mangaLink) {
+            add.Add(mangaName, mangaLink);
+        });
+    });
 }
 
 exports.RemoveManga = function RemoveManga() {
@@ -39,12 +44,11 @@ exports.SearchManga = function SearchManga() {
     common.rl.question(`${common.colors.cyan}If can't find chapter, search again? Yes | No\n`, function(yesno) {
         if (!common.qYesNo(yesno)) { // negative form
             console.log(common.colors.yellow, "Ok, won't search again.");
-            mangas.search(main.jsonManga, false);
+            search.Search(main.jsonManga, false);
         }
         if (common.qYesNo(yesno)) { // affirmative form
             console.log(common.colors.yellow, "Ok, will search again if necessary.");
-            mangas.search(main.jsonManga, true);
+            search.Search(main.jsonManga, true);
         }
     });
-    //common.WhatNow('search');
 }
